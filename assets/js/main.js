3955d650113ed254a05bd7619faadb699538315c
GabrielVanco <gabrielvanco111223@gmail.com>
2026-02-02 19:36:45 -0300

    Criando funções atuxiliares de atualizar e limpar o display. Função de apagar caracteres. Função para adicionar mais caracteres e restrições para não adicionar mais de uma virgula

---
+++ assets/js/main.js
@@ -0,0 +1,50 @@
+let displayValor = "0";
+let operadorAnterior = null;
+let aguardandoNovoNumero = false;
+
+function atualizarDisplay() {
+  document.getElementById("display").textContent = displayValor;
+}
+
+function limpar() {
+  displayValor = "0";
+  operadorAnterior = null;
+  aguardandoNovoNumero = false;
+  atualizarDisplay();
+}
+
+function apagar() {
+  if (displayValor.length > 1) {
+    displayValor = displayValor.slice(0, -1);
+  } else {
+    displayValor = "0";
+  }
+  atualizarDisplay();
+}
+
+function adicionarCaractere(caractere) {
+    // Se está aguardando novo númrero, substitui o display 
+    if(aguardandoNovoNumero) {
+        if(caractere === '.') {
+            displayValor = '0.';
+        } else if(['+', '*', '/', '%'].includes(caractere)) {
+            displayValor += caractere;
+        } else {
+            displayValor = false;
+        }
+        aguardandoNovoNumero = false;
+    } else {
+        // Se o display é '0' e não é um ponto, substitui
+        if (displayValor === '0' && caractere  !== '.'){
+            displayValor = caractere;
+        } else {
+            // Não permite múltiplos pontos decimais no mesmo número
+            const ultimoNumero = displayValor.split(/[\+\*\%\/]/).pop();
+            if(caractere === '.' && ultimoNumero.includes('.')){
+                return;
+            }
+            displayValor += caractere;
+        }
+    }
+    atualizarDisplay();
+}
